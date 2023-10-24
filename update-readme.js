2a2adec0090a8cf06020e96bb2eca2cae503d148
Shivam Upadhyay <alienc512345@gmail.com>
2023-10-24 23:04:56 +0530

    test status

--- update-readme.js
+++ update-readme.js
@@ -1,25 +1,29 @@
 const now = new Date();
-const currentHour = now.getHours();
-const dayOfWeek = now.getDay();
+const timezoneOffset = 5.5; // IST (Indian Standard Time) is UTC+5:30
+const utc = now.getTime() + (now.getTimezoneOffset() * 60000);
+const currentIST = new Date(utc + (3600000 * timezoneOffset));
+
+const currentHour = currentIST.getHours();
+const dayOfWeek = currentIST.getDay();
 
 let status = '';
 
 if (dayOfWeek >= 1 && dayOfWeek <= 5) { // Monday to Friday
     if (currentHour >= 10 && currentHour < 20) {
-        status = 'Working/coding...';
+        status = 'Working';
     }
 } else if (dayOfWeek === 0 || dayOfWeek === 6) { // Sunday or Saturday
-    status = 'Chilling Out or Coding...';
+    status = 'Fun';
 }
 
 if (currentHour >= 7 && currentHour < 8) {
     status = 'Working Out';
 } else if (currentHour >= 20 && currentHour < 22) {
-    status = 'With Family...';
+    status = 'With Family';
 } else if (currentHour >= 22 && currentHour < 23) {
-    status = 'Enjoying...';
-} else if ((currentHour >= 23 && now.getMinutes() >= 0) || (currentHour >= 0 && currentHour < 6 && now.getMinutes() < 30)) {
-    status = 'Sleeping...';
+    status = 'Entertainment';
+} else if (currentHour >= 23 || (currentHour >= 0 && currentHour < 6 && currentIST.getMinutes() < 30)) {
+    status = 'Sleeping';
 }
 
 const fs = require('fs');
